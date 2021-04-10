bb91aab6629b211119c4826846d85d0410c41d14
Sebastian H <sebastianhenaoloaiza@gmail.com>
2021-04-10 16:03:23 -0500

    Ejercicio 2 CORREGIR NUMERO MAS CERCANO AL PROMEDIO

--- Ejercicios/src/taller_2/Ejercicio_1.java
+++ Ejercicios/src/taller_2/Ejercicio_1.java
@@ -1,10 +1,30 @@
 package taller_2;
 
+import javax.swing.*;
+import java.util.Arrays;
+
 public class Ejercicio_1 {
     public static void main(String[] args) {
-        int numero_enteros = 0;
+        int numero_enteros = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de numeros a almacenar: "));
         int vector[] = new int [numero_enteros];
-        double promedio;
+        double acumulador = 0, promedio = 0, cercano = 0;
 
+        for(int i = 0; i < numero_enteros; i++){
+            vector[i] =  Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero "+ i + ": "));
+            acumulador = acumulador + vector[i];
+        }
+
+        promedio = acumulador / numero_enteros;
+
+        /* Este codigo fue sacado de la web:
+        https://www.sololearn.com/Discuss/2368527/como-obtengo-el-numero-mas-cercano-de-un-promedio-en-java*/
+        for(int x = 0; x < numero_enteros - 1; x++){
+            if (promedio - vector[x] > promedio - vector[x + 1]){
+                cercano = vector[x + 1];
+            }
+        }
+        /*---------------------------------------------------------*/
+
+        JOptionPane.showMessageDialog(null,"Promedio: " + promedio + "\nNumero cercano al promedio: " + cercano );
     }
 }
