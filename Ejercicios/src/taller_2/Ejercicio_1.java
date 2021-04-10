53e27b28e422f67cc90e5b0ce933dd077e512486
Sebastian H <sebastianhenaoloaiza@gmail.com>
2021-04-10 15:30:19 -0500

    Nuevo Taller

---
+++ Ejercicios/src/taller_2/Ejercicio_1.java
@@ -0,0 +1,10 @@
+package taller_2;
+
+public class Ejercicio_1 {
+    public static void main(String[] args) {
+        int numero_enteros = 0;
+        int vector[] = new int [numero_enteros];
+        double promedio;
+
+    }
+}
---
+++ Ejercicios/src/taller_2/menu.java
@@ -0,0 +1,7 @@
+package taller_2;
+
+public class menu {
+    public static void main(String[] args) {
+
+    }
+}
